fa28a7263ab8c4467f8d61f4e8e86e27942f5fef
wang <564873975@qq.com>
2015-11-24 11:12:49 +0800

    新增 爱心公益页面

---
+++ application/views/about/axgy_ny.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>聚雪球_网加金服_P2P理财首选互联网金融P2P网贷平台_100元即可投资!</title>
+    <?php $this->load->view('common/head_file'); ?>
+</head>
+<body>
+<?php $this->load->view('common/head'); ?>
+    <div class="axgy_sy">
+        <div class="user_nav row">
+            <a href="">首页</a>&nbsp;&gt;&nbsp;<a href="">爱心公益</a>&nbsp;&gt;&nbsp;<a href="">龚小美</a>
+        </div>
+        <div class="row">
+            <div class="left_axgy_sy fl">
+                <div class="zxbd">
+                    <div class="zxbd_body zxbd_body_1">
+                        <div class='img fl'><a href=""><img src="/assets/images/axgy/axgy_mr/4.jpg" alt=""></a></div>
+                        <div class='nr fl'>
+                            <h6><a href=""><font>第八期</font>最美的“女汉子”</a></h6>
+                            <div>
+                                <em>受助人信息</em>
+                                <p>姓名：龚小美<font class="fr">申请日期：2015-10-20</font></p>
+                                <p>年龄：6岁<font class="fr">申请原因：家境贫寒</font></p>
+                            </div>
+                            <div>
+                                <em>受助人简介</em>
+                                &nbsp;&nbsp;&nbsp;&nbsp;小美同学拥有强烈的自学意识，学习成绩一直位列年级前茅。该同学家住广东省河源市连平县油溪镇上镇村油镇，是单亲家庭。父亲不仅患病，而且腿脚不便。该同学有一个比自己小4岁妹妹，两姐妹与父亲相依为命。
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="axxm">
+                    <h1 class="bor"><i class="xxjs"></i><font>详情介绍</font></h1>
+                    <div class="axxq">
+                        <p>2011年4月2日， 爱佑慈善基金会前身为北京市华夏慈善基金会，是2004年《基金会管理条例》出台后国内第一家注册成立的非公募基金会。2008年初，在国家民政部的建议下，经国务院审批，基金会由地方性升级为全国性非公募基金会。爱佑慈善基金会以非公募形式筹集善款，以慈善家的心推动财富的理性再分配，在贫富两极之间架起扶弱济贫的桥梁。按照发起理事的设想，爱佑慈善基金会的目标是成为专注于特定领域的专业基金会。因此在基金会成立之初，通过调研各类慈善项目，对社会不同弱势群体进行分类评估，把基金会的救助对象锁定为孤贫儿童。</p>
+                        <p>治疗贫困地区的、患有动脉导管关闭、室间隔缺损、房间隔缺损、肺动脉狭窄以及多种合并症、法乐氏四联症等各种类型的先天性心脏病儿童。随着我国小儿先天性心脏病诊疗技术的提高，在大型医疗专科中心，大部分患有先天性心脏病的婴幼儿都可以通过手术治愈，并能够完全达到或接近正常人的手术水平。</p>
+                        <p>为了确保救助款安全、高效的使用，基金会依据各地实际情况，为该项目设计了严密的闭环流程，力图做到对患儿信息收集和筛选、核实，救助款发放、使用、患儿家庭抽查、手术效果追踪反馈等各个环节进行有效的监控，杜绝救助款的流失与滥用。基金会能够对救助款的使用进行实时监控，确保救助款的使用安全；受助患儿的筛选形成患儿家庭提交材料，政府出具证明，定点医院核实病情、基金会审核的多重把关，最大限度的保证只有贫困患儿才能够得到救助。</p>
+                    </div>
+                </div>
+                <div class="axxm">
+                    <h1><i class="jzjl"></i><font>捐助记录</font></h1>
+                    <div class="axxq" style="padding:0px;">
+                        <p class="title eee"><font class="tl">捐款人</font><font class="tc">捐款时间</font><font class="tr">金额（元）</font></p>
+                        <p class="fff"><font class="tl">李荣集</font><font class="tc">2015-10-28 11:35:22</font><font class="tr">10,000.00</font></p>
+                        <p class="eee"><font class="tl">李荣集</font><font class="tc">2015-10-28 11:35:22</font><font class="tr">10,000.00</font></p>
+                        <p class="fff"><font class="tl">李荣集</font><font class="tc">2015-10-28 11:35:22</font><font class="tr">10,000.00</font></p>
+                        <p class="eee"><font class="tl">李荣集</font><font class="tc">2015-10-28 11:35:22</font><font class="tr">10,000.00</font></p>
+                        <p class="fff"><font class="tl">李荣集</font><font class="tc">2015-10-28 11:35:22</font><font class="tr">10,000.00</font></p>
+                        <p class="eee"><font class="tl">李荣集</font><font class="tc">2015-10-28 11:35:22</font><font class="tr">10,000.00</font></p>
+                    </div>
+                    <div class="invest_home_paging tc">
+                        <span class="nocli pre">上一页</span><font class="active">1</font><font>2</font><font>3</font><font>4</font><font>5</font><font>6</font><span class="next">下一页</span>
+                    </div>
+                </div>
+                
+            </div>
+            <div class="right_axgy_sy fr">
+                <div class="jzcell">
+                    <p class="sy">剩余可捐助金额</p>
+                    <p class="syje">1,006,000.00<font>元</font></p>
+                    <p class="jdt">
+                        <span><span style="width:30%;"></span></span>
+                    </p>
+                    <p class="xmjd fr">项目进度：34%</p>
+                    <p class="jzje">捐助金额<font class="jzzjsr">20元</font><font class="jzzjsr selected">50元</font><font class="jzzjsr">100元</font></p>
+                    <p class="jzje_inp">自定义金额<input class="jzzjsr" type="text" placeholder="请输入捐助金额">元</p>
+                    <button type="">立即捐助</button>
+                </div>
+                <div class="ssaxb xgbd">
+                    <h1><i class="xgbd"></i><font>相关报道</font><a href="">查看全部报道</a></h1>
+                    <p><a href="">【沈阳晚报】最美的“女汉子”</a></p>
+                </div>
+                <div class="ssaxb cjwt">
+                    <h1><i class="cjwt"></i><font>相关报道</font><a href="">查看全部报道</a></h1>
+                    <p>
+                        <font>我捐助的钱最终用到哪里？</font>
+                        <span class="pop_axcjwt">
+                            <span class="titl">
+                                我捐助的钱最终用到哪里？
+                                <em class="fr">关闭</em>
+                            </span>
+                            <span class="pop_axcjwt_body">
+                            <em></em>
+                            怎么跟你形容呢？就这么跟你说吧，你所捐助的每一分钱，都将为我手里的土豪金苹果6s plus做出一份不可磨灭贡献。所以各位土豪，展现你们实力的时候到了！各位有钱捧个钱场，没钱捧个人场，我下半生的幸福生活就看各位今天的表现了。
+                            </span>
+                        </span>
+                    </p>
+                    <p>
+                        <font>我最多/最少可以捐多少钱？</font>
+                        <span class="pop_axcjwt">
+                            <span class="titl">
+                                我捐助的钱最终用到哪里？
+                                <em class="fr">关闭</em>
+                            </span>
+                            <span class="pop_axcjwt_body">
+                            <em></em>
+                            怎么跟你形容呢？就这么跟你说吧，你所捐助的每一分钱，都将为我手里的土豪金苹果6s plus做出一份不可磨灭贡献。所以各位土豪，展现你们实力的时候到了！各位有钱捧个钱场，没钱捧个人场，我下半生的幸福生活就看各位今天的表现了。
+                            </span>
+                        </span>
+                    </p>
+                    <p>
+                        <font>在聚雪球捐款会有荣誉证书吗？</font>
+                        <span class="pop_axcjwt">
+                            <span class="titl">
+                                我捐助的钱最终用到哪里？
+                                <em class="fr">关闭</em>
+                            </span>
+                            <span class="pop_axcjwt_body">
+                            <em></em>
+                            怎么跟你形容呢？就这么跟你说吧，你所捐助的每一分钱，都将为我手里的土豪金苹果6s plus做出一份不可磨灭贡献。所以各位土豪，展现你们实力的时候到了！各位有钱捧个钱场，没钱捧个人场，我下半生的幸福生活就看各位今天的表现了。
+                            </span>
+                        </span>
+                    </p>
+                    <p>
+                        <font>我可以不通过网上银行捐款吗？</font>
+                        <span class="pop_axcjwt">
+                            <span class="titl">
+                                我捐助的钱最终用到哪里？
+                                <em class="fr">关闭</em>
+                            </span>
+                            <span class="pop_axcjwt_body">
+                            <em></em>
+                            怎么跟你形容呢？就这么跟你说吧，你所捐助的每一分钱，都将为我手里的土豪金苹果6s plus做出一份不可磨灭贡献。所以各位土豪，展现你们实力的时候到了！各位有钱捧个钱场，没钱捧个人场，我下半生的幸福生活就看各位今天的表现了。
+                            </span>
+                        </span>
+                    </p>
+                    <p>
+                        <font>我可以实时追踪我的捐款吗？</font>
+                        <span class="pop_axcjwt">
+                            <span class="titl">
+                                我捐助的钱最终用到哪里？
+                                <em class="fr">关闭</em>
+                            </span>
+                            <span class="pop_axcjwt_body">
+                            <em></em>
+                            怎么跟你形容呢？就这么跟你说吧，你所捐助的每一分钱，都将为我手里的土豪金苹果6s plus做出一份不可磨灭贡献。所以各位土豪，展现你们实力的时候到了！各位有钱捧个钱场，没钱捧个人场，我下半生的幸福生活就看各位今天的表现了。
+                            </span>
+                        </span>
+                    </p>
+                </div>
+                <div class="wqbd gdxm">
+                    <h1><i class="gdxm"></i><font>更多爱心项目</font><a href="">查看全部报道</a></h1>
+                    <ul>
+                        <li>
+                            <div class='img fl'><a href=""><img src="../../../../assets/images/axgy/axgy_mr/5.jpg" alt=""><font>第十期</font></a></div>
+                            <div class='nr fl'>
+                                <p><font>姓名：</font>张芬</p>
+                                <p><font>年龄：</font>6岁</p>
+                                <p><font>申请日期：</font>2015-10-20</p>
+                                <p><font>申请原因：</font>家境贫寒，无劳动力</p> 
+                            </div>
+                        </li>
+                        <li>
+                            <div class='img fl'><a href=""><img src="../../../../assets/images/axgy/axgy_mr/5.jpg" alt=""><font>第十期</font></a></div>
+                            <div class='nr fl'>
+                                <p><font>姓名：</font>张芬</p>
+                                <p><font>年龄：</font>6岁</p>
+                                <p><font>申请日期：</font>2015-10-20</p>
+                                <p><font>申请原因：</font>家境贫寒，无劳动力</p> 
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+<!--footer start-->
+<?php $this->load->view('common/footer'); ?>
+<!--footer end-->
+<script type="text/javascript">
+    $('.jzcell').find('.jzzjsr').click(function () {
+        // body...
+        $('.jzzjsr').removeClass('selected');
+        $(this).addClass('selected')
+    })
+    $('.cjwt').find('font').hover(
+        // body...
+        function () {
+            $(".pop_axcjwt").stop(false,true);
+            $(this).parent().find('.pop_axcjwt').fadeIn();
+        },
+        function () {
+            $(this).parent().find('.pop_axcjwt').fadeOut();
+        }
+    ) 
+</script>
+</body>
+</html>
---
+++ application/views/about/axgy_sy.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>聚雪球_网加金服_P2P理财首选互联网金融P2P网贷平台_100元即可投资!</title>
+    <?php $this->load->view('common/head_file'); ?>
+</head>
+<body>
+<?php $this->load->view('common/head'); ?>
+    <div class="axgy_sy">
+        <div class="banner_axgy_sy">
+            <div class="row">
+                <div class="fr kuang">
+                    <p>累计捐款<input type="text" value="1029537" disabled="disabled"/>元</p>
+                    <p>爱心人士<input type="text" value="56411" disabled="disabled"/>人</p>
+                    <p>爱心帮助<input type="text" value="435" disabled="disabled"/>人</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="left_axgy_sy fl">
+                <div class="zxbd">
+                    <h1><i></i><font>最新报道</font></h1>
+                    <div class="zxbd_body">
+                        <div class='img fl'><a href="<?php echo site_url('about/commonweal_info?id=1'); ?>"><img src="../../../../assets/images/axgy/axgy_mr/1.jpg" alt=""></a></div>
+                        <div class='nr fl'>
+                            <h2><a href="<?php echo site_url('about/commonweal_info?id=1'); ?>"><font>【第九期】</font>我一岁的时候妈妈就走了</a></h2>
+                            <p>
+                                “我一岁的时候妈妈就走了……”八岁的小芬说这句话的时候，眼睛望向墙角，有些失落。不知道周围人多少遍的重复，才能让她对一岁的“记忆”如此深刻。
+                            </p>
+                            <a class="ckxx" href="<?php echo site_url('about/commonweal_info?id=1'); ?>">查看详细>>></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="axxm">
+                    <h1><i></i><font>爱心项目</font><a href="">聚雪球公益基金介绍</a></h1>
+                    <div class="axxm_body">
+                        <ul>
+                            <li>
+                                <div class='img'><a href="<?php echo site_url('about/commonweal_info?id=1'); ?>"><img src="../../../../assets/images/axgy/axgy_mr/2.jpg" alt=""><font>第十期</font></a></div>
+                                <div class='nr'>
+                                    <h3><font>王小小</font>莲花乡中心小学二年级学生</h3>
+                                    <p>
+                                    小小的父亲和叔叔早年间都相继离世，妈妈难以承受如此家庭变故，精神分裂，离家出走。
+                                    </p>
+                                    <span>已有<font>1120</font>人捐款</span>
+                                </div>
+                                <a class="button" href="<?php echo site_url('about/commonweal_info?id=1'); ?>">立即捐助</a>
+                            </li>
+                            <li>
+                                <div class='img'><a href="<?php echo site_url('about/commonweal_info?id=1'); ?>"><img src="../../../../assets/images/axgy/axgy_mr/2.jpg" alt=""><font>第十期</font></a></div>
+                                <div class='nr'>
+                                    <h3><font>王小小</font>莲花乡中心小学二年级学生</h3>
+                                    <p>
+                                    小小的父亲和叔叔早年间都相继离世，妈妈难以承受如此家庭变故，精神分裂，离家出走。
+                                    </p>
+                                    <span>已有<font>1120</font>人捐款</span>
+                                </div>
+                                <a class="button zxz" href="">执行中</a>
+                            </li>
+                            <li class="mar_ri_0">
+                                <div class='img'><a href=""><img src="../../../../assets/images/axgy/axgy_mr/2.jpg" alt=""><font>第十期</font></a></div>
+                                <div class='nr'>
+                                    <h3><font>王小小</font>莲花乡中心小学二年级学生</h3>
+                                    <p>
+                                    小小的父亲和叔叔早年间都相继离世，妈妈难以承受如此家庭变故，精神分裂，离家出走。
+                                    </p>
+                                    <span>已有<font>1120</font>人捐款</span>
+                                </div>
+                                <a class="button zxwc" href="<?php echo site_url('about/commonweal_info?id=1'); ?>">捐助已完成</a>
+                            </li>
+                            <li>
+                                <div class='img'><a href="<?php echo site_url('about/commonweal_info?id=1'); ?>"><img src="../../../../assets/images/axgy/axgy_mr/2.jpg" alt=""><font>第十期</font></a></div>
+                                <div class='nr'>
+                                    <h3><font>王小小</font>莲花乡中心小学二年级学生</h3>
+                                    <p>
+                                    小小的父亲和叔叔早年间都相继离世，妈妈难以承受如此家庭变故，精神分裂，离家出走。
+                                    </p>
+                                    <span>已有<font>1120</font>人捐款</span>
+                                </div>
+                                <a class="button" href="<?php echo site_url('about/commonweal_info?id=1'); ?>">立即捐助</a>
+                            </li>
+                            <li>
+                                <div class='img'><a href="<?php echo site_url('about/commonweal_info?id=1'); ?>"><img src="../../../../assets/images/axgy/axgy_mr/2.jpg" alt=""><font>第十期</font></a></div>
+                                <div class='nr'>
+                                    <h3><font>王小小</font>莲花乡中心小学二年级学生</h3>
+                                    <p>
+                                    小小的父亲和叔叔早年间都相继离世，妈妈难以承受如此家庭变故，精神分裂，离家出走。
+                                    </p>
+                                    <span>已有<font>1120</font>人捐款</span>
+                                </div>
+                                <a class="button" href="<?php echo site_url('about/commonweal_info?id=1'); ?>">立即捐助</a>
+                            </li>
+                            <li class="mar_ri_0">
+                                <div class='img'><a href="<?php echo site_url('about/commonweal_info?id=1'); ?>"><img src="../../../../assets/images/axgy/axgy_mr/2.jpg" alt=""><font>第十期</font></a></div>
+                                <div class='nr'>
+                                    <h3><font>王小小</font>莲花乡中心小学二年级学生</h3>
+                                    <p>
+                                    小小的父亲和叔叔早年间都相继离世，妈妈难以承受如此家庭变故，精神分裂，离家出走。
+                                    </p>
+                                    <span>已有<font>1120</font>人捐款</span>
+                                </div>
+                                <a class="button" href="<?php echo site_url('about/commonweal_info?id=1'); ?>">立即捐助</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="invest_home_paging tc">
+                        <span class="nocli pre">上一页</span><font class="active">1</font><font>2</font><font>3</font><font>4</font><font>5</font><font>6</font><span class="next">下一页</span>
+                </div>
+            </div>
+            <div class="right_axgy_sy fr">
+                <div class="ssaxb">
+                    <h1><i></i><font>实时爱心榜</font></h1>
+                    <p><font>杨天妃 捐助</font><a href="">脑瘫儿脚尖的艺术梦</a>1500元<span>2015-11-13</span></p>
+                    <p><font>杨天妃 捐助</font><a href="">脑瘫儿脚尖的艺术梦</a>1500元<span>2015-11-13</span></p>
+                    <p><font>杨天妃 捐助</font><a href="">脑瘫儿脚尖的艺术梦</a>1500元<span>2015-11-13</span></p>
+                    <p><font>杨天妃 捐助</font><a href="">脑瘫儿脚尖的艺术梦</a>1500元<span>2015-11-13</span></p>
+                    <p><font>杨天妃 捐助</font><a href="">脑瘫儿脚尖的艺术梦</a>1500元<span>2015-11-13</span></p>
+                    <p><font>杨天妃 捐助</font><a href="">脑瘫儿脚尖的艺术梦</a>1500元<span>2015-11-13</span></p>
+                </div>
+                <div class="wqbd">
+                    <h1><i></i><font>往期报道</font><a href="">查看全部报道</a></h1>
+                    <ul>
+                        <li>
+                            <h2><a href=""><font>【第九期】</font>我一岁的时候妈妈就走了</a></h2>
+                            <div class='img fl'><a href=""><img src="../../../../assets/images/axgy/axgy_mr/3.jpg" alt=""></a></div>
+                            <div class='nr fl'>
+                                <p>在一些人的眼里，残疾人会因为自身的原因而自暴自弃，但是小宝却没有……<a href="">[详细]</a></p>
+                                <font>2015-10-15</font>
+                            </div>
+                        </li>
+                        <li>
+                            <h2><a href=""><font>【第九期】</font>我一岁的时候妈妈就走了</a></h2>
+                            <div class='img fl'><a href=""><img src="../../../../assets/images/axgy/axgy_mr/3.jpg" alt=""></a></div>
+                            <div class='nr fl'>
+                                <p>在一些人的眼里，残疾人会因为自身的原因而自暴自弃，但是小宝却没有……<a href="">[详细]</a></p>
+                                <font>2015-10-15</font>
+                            </div>
+                        </li>
+                        <li>
+                            <h2><a href=""><font>【第九期】</font>我一岁的时候妈妈就走了</a></h2>
+                            <div class='img fl'><a href=""><img src="../../../../assets/images/axgy/axgy_mr/3.jpg" alt=""></a></div>
+                            <div class='nr fl'>
+                                <p>在一些人的眼里，残疾人会因为自身的原因而自暴自弃，但是小宝却没有……<a href="">[详细]</a></p>
+                                <font>2015-10-15</font>
+                            </div>
+                        </li>
+                        <li>
+                            <h2><a href=""><font>【第九期】</font>我一岁的时候妈妈就走了</a></h2>
+                            <div class='img fl'><a href=""><img src="../../../../assets/images/axgy/axgy_mr/3.jpg" alt=""></a></div>
+                            <div class='nr fl'>
+                                <p>在一些人的眼里，残疾人会因为自身的原因而自暴自弃，但是小宝却没有……<a href="">[详细]</a></p>
+                                <font>2015-10-15</font>
+                            </div>
+                        </li>
+                        <li>
+                            <h2><a href=""><font>【第九期】</font>我一岁的时候妈妈就走了</a></h2>
+                            <div class='img fl'><a href=""><img src="../../../../assets/images/axgy/axgy_mr/3.jpg" alt=""></a></div>
+                            <div class='nr fl'>
+                                <p>在一些人的眼里，残疾人会因为自身的原因而自暴自弃，但是小宝却没有……<a href="">[详细]</a></p>
+                                <font>2015-10-15</font>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+<!--footer start-->
+<?php $this->load->view('common/footer'); ?>
+<!--footer end-->
+</body>
+</html>
