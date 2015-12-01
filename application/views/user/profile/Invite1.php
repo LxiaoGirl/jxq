e73a02b9179a0b3b9466d9cb250b13df865c86b3
wang <564873975@qq.com>
2015-12-01 20:56:44 +0800

    2015-12.01-添加-

---
+++ application/views/user/profile/Invite1.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>聚雪球_网加金服_P2P理财首选互联网金融P2P网贷平台_100元即可投资!</title>
+    <?php $this->load->view('common/head_file'); ?>
+		<script type="text/javascript" src="<?php echo base_url('assets/js/plugins/qrcode.js')?>"></script>
+	<script>
+    window.onload = function () {
+        var qrcode = new QRCode(document.getElementById("qrcode"), {
+            width: 170,//设置宽高
+            height: 150
+        });
+        qrcode.makeCode("https://www.zgwjjf.com/index.php/jujianren/jieshao?inviter_no=<?php echo $invite_code?>");
+    }
+</script>
+</head>
+<body>
+<!--head start-->
+<?php $this->load->view('common/head');?>       
+    <!--head end-->
+    <!--user start-->
+    <div class="user_nav row">
+        <a href="">首页</a>&nbsp;>&nbsp;<a href="">账户设置</a>&nbsp;>&nbsp;<a href="">邀请好友</a>
+    </div>
+    <div class="row user">
+        <!--左侧通用-->
+        <?php $this->load->view('common/user_left');?>
+        <!--右侧-->
+        <div class="user_right">
+            <div class="my_level">
+                <div class="h1">
+                    <p class="top">邀请好友<font>当前居间人等级：<span>VIP<font><?php echo ($lv)?$lv:0?></font></span></font><a href="">如何升级？</a><a href="">我能拿的提成是多少？</a></p>
+                    <p class="bot"><font>已结算总额（元）：</font><?php echo $jujian_amount['data']['jujian_amount']?></p>
+                    <div class="ewm">
+                        <div class="ewm_but">查看我的二维码</div>
+                        <div class="ewm_pop"  id="qrcode"></div>
+                    </div>
+                </div>
+                <p class="hbmx">居间收益明细</p>
+                <ul class="tab_title ">
+                    <li><a href="<?php echo site_url('user/user/invite');?>">居间人账户收益</a><font class="fr">|</font></li>
+                    <li class="active">客户列表</li>
+                </ul>
+                <ul class="tab_con invite">
+                    <li class="active">
+					<?php if($jujian['status']=='10000'):?>
+					<?php foreach($jujian['data']['data'] as $k => $v):?>
+                        <div class="month_lie" id="123">
+                            <div class="kuhf_pos_a">
+                                <div class="kh_tx"><img src="<?php echo base_url('assets/images/user/mrtx.png')?>"></div>
+                                <h5><?php echo $v['user_name']?></h5>
+								<h5><?php echo $v['ralation']?></h5>
+                            </div>
+                            <div class="tit_k">
+                                <div class="fl">
+                                    <h5 class="linh_gg"><font>累计投资金额金额（元）:</font><?php echo $v['amount']?></h5>
+                                    <p>最近一次登录时间：<?php echo (my_date($v['last_date'],2))?my_date($v['last_date'],2):'很久没有登录过了哦'?></p>
+                                </div>
+                                <div class="fr">
+                                    <button class="invite_ckxq" id="<?php echo $v['uid']?>">查看详情<em><em></em></em></button>
+                                </div>
+                            </div>
+                            <div class="month_lie_pop">                         
+                            </div>
+                        </div>
+						<?php endforeach;?>
+						<?php endif;?>
+                    </li>
+					<?php echo (isset($links))?$links:'';?>
+                </ul>
+            </div>
+        </div>
+        <!--右侧-->
+    </div>
+    <!--底部-->
+<?php $this->load->view('common/footer');?> 
+    <!--底部-->       
+
+<!--userjs start-->
+<script type="text/javascript">
+    seajs.use(['jquery','sys'],function(){
+        $('.ewm_but').hover(function  () {
+            $('.ewm_pop').slideToggle();
+        });
+        $('.invite_ckxq').click(function  () {
+			var e = $(this);
+			$.post('/index.php/user/user/get_commission_list?uid='+$(this).attr('id'),{},function(result){
+				    var text = '<p class="month_lie_poptitle">';
+					text=text+'<span class="tl wid_159">投资时间</span>';
+					text=text+'<span class="tl wid_159">项目名称</span>';
+					text=text+'<span class="tl wid_108">项目状态</span>';
+					text=text+'<span class="tr wid_125">投资金额（元）</span>';
+					text=text+'</p>';
+					result = JSON.parse(result);
+					for(var i=0;i<result.data.length;i++){
+						if(result.data[i].status==4){
+							var status = '还款中';
+						}
+						if(result.data[i].status==7){
+							var status = '还款完成';
+						}
+						text = text+'<p class="month_lie_popnr">';
+						text=text+'<span class="tl wid_159">'+result.data[i].dateline+'</span>';
+						text=text+'<span class="tl wid_159">'+result.data[i].subject_1+'</span>';
+						text=text+'<span class="tl wid_108">'+status+'</span>';
+						text=text+'<span class="tr wid_125">'+result.data[i].invest_amount+'</span>';
+						text=text+'</p>';
+					}
+					e.parent().parent().parent().find('.month_lie_pop').html(text);
+				});			
+            $(this).toggleClass('select');
+            $(this).parent().parent().parent().find('.month_lie_pop').slideToggle();
+        })
+    });
+</script>
+</body> 
+</html>
