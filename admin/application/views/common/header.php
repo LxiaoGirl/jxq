efbe37e971c89133b382c73f6f05a125cca8c64e
wang <564873975@qq.com>
2015-12-21 15:37:12 +0800

    过滤admin

--- admin/application/views/common/header.php
+++
@@ -1,7 +0,0 @@
-<link rel="stylesheet" href="/admin/assets/css/styles.css">
-<!--[if lt IE 9]>
-<link rel="stylesheet" href="/admin/assets/css/ie8.css">
-<script type="text/javascript" src="/admin/assets/js/html5.js"></script>
-<script type="text/javascript" src="/admin/assets/js/respond.min.js"></script>
-<script type="text/javascript" src="/admin/assets/plugins/charts-flot/excanvas.min.js"></script>
-<![endif]-->
