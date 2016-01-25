e320dc3273cf882580f92dd07202df9a8d0306d6
wang-mac <564873975@qq.com>
2016-01-25 12:01:13 +0800

    2016.1.25-修改－活动主页标题修改

--- application/views/mobiles/wish/home.php
+++ application/views/mobiles/wish/home.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>新年愿望</title>
+    <title>新年红包任性撒，不要白不要</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <!--H5页面窗口自动调整到设备宽度，并禁止用户缩放页面-->
